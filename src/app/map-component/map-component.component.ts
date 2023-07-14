669280e5fed3af2e0f5764541efb80709d8d624b
Anisha Jeni Ravi Sam <jenisam45454@gmail.com>
2023-07-14 04:04:53 +0100

    BG set

--- src/app/map-component/map-component.component.ts
+++ src/app/map-component/map-component.component.ts
@@ -9,18 +9,41 @@ export class MapComponentComponent {
 
   map: number[][] = [];
 
+  objectTypes = {
+    farm: 0,
+    goldMine: 1,
+    building: 2,
+    docks: 3,
+    // Add more types as needed
+  };
+  
   ngOnInit() {
     // Generate a 2D map with random values
     const numRows = 20;
     const numCols = 20;
 
+    // Initialize the array with default values (e.g., -1)
     for (let i = 0; i < numRows; i++) {
-      const row = [];
-      for (let j = 0; j < numCols; j++) {
-        row.push(Math.floor(Math.random() * 10)); // Generate a random value (0-9)
-      }
+      const row: number[] = Array(numCols).fill(-1);
       this.map.push(row);
     }
+    // Place objects randomly in n locations
+    let count = 0;
+    while (count < 75) {
+      const randomRow = Math.floor(Math.random() * numRows);
+      const randomCol = Math.floor(Math.random() * numCols);
+
+      if (this.map[randomRow][randomCol] === -1) {
+        // Generate a random object type index from 0 to number of types - 1
+        const randomTypeIndex = Math.floor(Math.random() * Object.keys(this.objectTypes).length);
+        
+        // Assign the object type to the location
+        this.map[randomRow][randomCol] = randomTypeIndex;
+
+        count++;
+      }
+    }
   }
+  
 
 }
